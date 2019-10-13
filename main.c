0611255c8a4d6cff01d173d24ae70e03a1403e74
seedmonn <seedmp322@yandex.ru>
2019-10-13 11:05:51 +0300

    Add project files.

---
+++ main.c
@@ -0,0 +1,42 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#define MAXLINE 1024
+
+int main(void)
+{
+	FILE* fpin;
+	FILE* fpout;
+
+	char line[MAXLINE];
+	char* ptr;
+	int N = 0;
+
+	fpin = fopen("text.txt", "rt");
+	if (fpin == NULL)
+		return;
+	fpout = fopen("result.txt", "wt");
+	if (fpout == NULL)
+		return;
+	printf("Enter N: \n");
+	while (!feof(fpin))
+	{
+		ptr = fgets(line, MAXLINE, fpin);
+		if (ptr == NULL)
+			break;
+
+		while (*ptr != '\0')
+		{
+			if (*ptr == ' ' || *ptr == '.' || *ptr == ',')
+			{
+				*ptr++ = '\n';
+				*ptr = '\0';
+				break;
+			}
+			ptr++;
+		}
+		fputs(line, fpout);
+	}
+	fclose(fpin);
+	fclose(fpout);
+	return 0;
+}
