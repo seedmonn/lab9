f92676986822fbe16fe709e242c5d6ae734c9fde
seedmonn <seedmp322@yandex.ru>
2019-10-15 22:25:42 +0300

    -stable version 1.0a

--- main.c
+++ main.c
@@ -1,36 +1,35 @@
 #pragma warning(disable:4996)
+#include <conio.h>
 #include <stdio.h>
-#define MAXLINE 1024
-
-int main(void)
+int main(int argc, char* argv[])
 {
-	FILE* fpin;
-	FILE* fpout;
-
-	char line[MAXLINE];
-	char* ptr;
-	int N = 0;
-
-	fpin = fopen("text.txt", "rt");
-	if (fpin == NULL)
-		return;
-	fpout = fopen("result.txt", "wt");
-	if (fpout == NULL)
-		return;
-	printf("Enter N: \n");
-	while (!feof(fpin))
+	FILE* f;
+	int slovo, N;
+	slovo = 1;
+	char ch;
+	if ((f = fopen("D:\\Text\\text1.txt", "r")) == NULL)
 	{
-		ptr = fgets(line, MAXLINE, fpin);
-		if (ptr == NULL)
-			break;
-
-		while ()
-		{
-			
-		}
-		fputs(line, fpout);
+		printf("Cannot open input file.\n");
+		return 1;
 	}
-	fclose(fpin);
+	printf("Please enter the N:");
+	scanf_s("%d", &N);
+	FILE* fpout;
+	fpout = fopen("D:\\Statistic\\Creator.txt", "w");
+	fprintf(fpout, "Result text file: \n");
+	while (!feof(f))
+	{
+		ch = fgetc(f);
+		if (N != slovo)
+		{
+			fprintf(fpout, "%c", ch);
+		}
+		if (ch == ' ' || ch == '\t')
+			slovo++;
+		if (ch == '\n')
+			slovo = 1;
+	}
 	fclose(fpout);
+	fclose(f);
 	return 0;
 }
