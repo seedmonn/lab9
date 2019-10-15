74fb90a27f551ee66705fede1d84fbbf7aa429cc
seedmonn <seedmp322@yandex.ru>
2019-10-15 22:35:11 +0300

    - added comments

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 	int slovo, N;
 	slovo = 1;
 	char ch;
-	if ((f = fopen("D:\\Text\\text1.txt", "r")) == NULL)
+	if ((f = fopen("D:\\Text\\text1.txt", "r")) == NULL) // Путь к файлу с текстом
 	{
 		printf("Cannot open input file.\n");
 		return 1;
@@ -15,16 +15,16 @@ int main(int argc, char* argv[])
 	printf("Please enter the N:");
 	scanf_s("%d", &N);
 	FILE* fpout;
-	fpout = fopen("D:\\Statistic\\Creator.txt", "w");
+	fpout = fopen("D:\\Statistic\\Creator.txt", "w"); // Путь к записи
 	fprintf(fpout, "Result text file: \n");
 	while (!feof(f))
 	{
 		ch = fgetc(f);
 		if (N != slovo)
 		{
-			fprintf(fpout, "%c", ch);
+			fprintf(fpout, "%c", ch); //Считает точки и переход на следущую строку.
 		}
-		if (ch == ' ' || ch == '\t')
+		if (ch == ' ' || ch == '\t') //Табуляцию тоже считаем.
 			slovo++;
 		if (ch == '\n')
 			slovo = 1;
