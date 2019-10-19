c7b664c1520c1e4658a2d04c30f2002b7554460e
seedmonn <seedmp322@yandex.ru>
2019-10-19 21:26:36 +0300

    fixed

--- main.c
+++ main.c
@@ -8,15 +8,15 @@ int main(int argc, char* argv[])
 	slovo = 1;
 	char ch;
 	char ch_gag;
-	if ((f = fopen("C:\\Users\\Admin\\source\\repos\\laba9\\text.txt", "r")) == NULL) // Путь к файлу с текстом
+	if ((f = fopen("C:\\Users\\Admin\\source\\repos\\laba9\\text.txt", "r")) == NULL) //
 	{
 		printf("Cannot open input file.\n");
 		return 1;
 	}
-	printf("Please enter the N:");
+	printf("Please enter the N: ");
 	scanf_s("%d", &N);
 	FILE* fpout;
-	fpout = fopen("C:\\Users\\Admin\\source\\repos\\laba9\\result.txt", "w"); // Путь к записи
+	fpout = fopen("C:\\Users\\Admin\\source\\repos\\laba9\\result.txt", "w"); //
 	fprintf(fpout, "Result text file: \n");
 	while (!feof(f))
 	{
@@ -25,10 +25,10 @@ int main(int argc, char* argv[])
 			break;
 		if ((N != slovo) || (ch == ' ' || ch == '\t' || ch == '\n'))
 		{
-			fprintf(fpout, "%c", ch); //Считает точки и переход на следущую строку.
+			fprintf(fpout, "%c", ch); // .
 		}
-		if ((ch == ' ' || ch == '\t' || ch == '\n') && (ch_gag != ' ' || ch_gag != '\t' || ch_gag != '\n')) //Табуляцию тоже считаем.
-			slovo++; 
+		if ((ch == ' ' || ch == '\t' || ch == '\n') && ch_gag != ' ' && ch_gag != '\t' && ch_gag != '\n') // .
+			slovo++;
 		if (ch == '\n')
 			slovo = 1;
 		ch_gag = ch;
